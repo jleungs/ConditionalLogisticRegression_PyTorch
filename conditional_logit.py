999fa135a3977eab06b78dc7bb8c108502d01a7f
Jonathan Leung <jl3@tuta.io>
2025-01-16 11:04:14 +0000

    added GPU support and as preferred

--- conditional_logit.py
+++ conditional_logit.py
@@ -6,9 +6,11 @@ class ConditionalLogisticRegression(torch.nn.Module):
     """
     Conditional / Fixed-Effects Logistic Regression model implemented with PyTorch.
     """
-    def __init__(self, X, y, strata, learning_rate=0.0001, max_iter=1000, groups_batch_size=5):
+    def __init__(self, X, y, strata, learning_rate=0.0001, max_iter=100, groups_batch_size=5):
         """ Initializing all neccessary variables """
         super(ConditionalLogisticRegression, self).__init__()
+        # try to use GPU
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         if isinstance(X, pd.DataFrame):
             X = X.values
@@ -22,8 +24,8 @@ class ConditionalLogisticRegression(torch.nn.Module):
             print(f"strata has to be one-dimensional, got {strata.shape[1]}")
             exit()
 
-        self.X = torch.tensor(X, dtype=torch.float32)
-        self.y = torch.tensor(y, dtype=torch.float32)
+        self.X = torch.tensor(X, dtype=torch.float32).to(self.device)
+        self.y = torch.tensor(y, dtype=torch.float32).to(self.device)
         self.strata_list = list(strata.groupby(strata.squeeze()).groups.values())
 
         if len(self.y.shape) > 1 and self.y.shape[1] != 1:
@@ -37,13 +39,13 @@ class ConditionalLogisticRegression(torch.nn.Module):
         input_size = self.X.shape[1]
         self.output_size = 1
 
-        self.linear = torch.nn.Linear(input_size, self.output_size, bias=True)
+        self.linear = torch.nn.Linear(input_size, self.output_size, bias=True).to(self.device)
 
 
     def forward(self, X, strata, train=True):
         """ Function to compute the probability, overwritten from torch.nn.Module """
-        y_hat = torch.exp(self.linear(X))
-        y_hat_sum = torch.empty(X.shape[0], self.output_size, dtype=torch.float32)
+        y_hat = torch.exp(self.linear(X)).to(self.device)
+        y_hat_sum = torch.empty(X.shape[0], self.output_size, dtype=torch.float32).to(self.device)
 
         if train:
             ix = 0
@@ -62,7 +64,7 @@ class ConditionalLogisticRegression(torch.nn.Module):
 
     def neg_log_likelihood(self, y_pred, y_true):
         """ The negative log-likelihood function to optimize """
-        return -torch.sum( y*(torch.log(self.y_hat) - torch.log(self.y_hat_sum)) )
+        return -(torch.sum(y_true * torch.log(y_pred)).to(self.device))
 
 
     def fit(self):
@@ -74,8 +76,6 @@ class ConditionalLogisticRegression(torch.nn.Module):
             random.shuffle(self.strata_list)
             # train on mini-batch
             for batch in range(0, len(self.strata_list), self.groups_batch_size):
-                # sets gradients to zero
-                sgd.zero_grad()
                 # select based on batch
                 strata_batch = self.strata_list[batch:batch+self.groups_batch_size]
                 # need to flat the list of lists to be able to get the indices
@@ -88,10 +88,11 @@ class ConditionalLogisticRegression(torch.nn.Module):
 
                 #strata_batch = strata_shuffled[batch:batch+batch_size]
                 # get probabilities
-                y_pred = self.forward(X_batch, strata_batch_len)
+                y_pred = self.forward(X_batch, strata_batch_len).to(self.device)
                 # negative log likelihood of predicted
                 loss = self.neg_log_likelihood(y_pred, y_batch)
                 # compute gradients and optimize
+                sgd.zero_grad()
                 loss.backward()
                 sgd.step()
 
@@ -113,7 +114,7 @@ class ConditionalLogisticRegression(torch.nn.Module):
             strata = pd.DataFrame(strata)
 
         with torch.no_grad():
-            X = torch.tensor(X, dtype=torch.float32)
+            X = torch.tensor(X, dtype=torch.float32).to(self.device)
             strata = strata.groupby(strata.squeeze()).groups.items()
 
             return self.forward(X, strata, train=False)
